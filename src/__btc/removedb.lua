bf7b0ce4a59723e6a72ac9c7847fcecf7863fa6d
cryi <cryi@tutanota.com>
2021-01-24 19:48:33 +0100

    updates to ami 0.6.0

--- src/__btc/removedb.lua
+++ src/__btc/removedb.lua
@@ -3,3 +3,4 @@ ami_assert(type(_user) == "string", "User not specified...")
 
 fs.remove("data/blocks", { recurse = true })
 fs.remove("data/chainstate", { recurse = true })
+log_success("Succesfully removed snowgemd database.")
